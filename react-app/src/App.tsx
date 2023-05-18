4bb6784637dd49adaba7c3d218474db7168c8cde
hipstar-08 <sonyjames9@gmail.com>
2023-05-18 08:56:33 +0530

    List changes removed

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,20 +1,5 @@
-import ListGroup from "./components/ListGroup";
-
 function App() {
-  let items = ["New York", "San, Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-  return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
