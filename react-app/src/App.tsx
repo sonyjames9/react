24709d0dc3e095d16ed56935796102aeae619698
hipstar-08 <sonyjames9@gmail.com>
2023-05-21 17:54:18 +0530

    button css

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,12 @@
 import ListGroup from "./components/ListGroup/ListGroup";
 import { BsFillCalendarFill } from "react-icons/bs";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
     <div>
-      <BsFillCalendarFill color="blue" size="20" />
+      {/* <BsFillCalendarFill color="blue" size="20" /> */}
+      <Button onClick={() => {}}>My Button</Button>
     </div>
   );
 }
--- react-app/src/components/Button/Button.tsx
+++ react-app/src/components/Button/Button.tsx
@@ -1,3 +1,5 @@
+import styles from "./Button.module.css";
+
 interface Props {
   children: string;
   color?: "primary" | "secondary" | "danger" | "success";
@@ -6,7 +8,10 @@ interface Props {
 
 const Button = ({ children, color = "primary", onClick }: Props) => {
   return (
-    <button className={"btn btn-" + color} onClick={onClick}>
+    <button
+      className={[styles.btn, styles["btn-" + color]].join(" ")}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
