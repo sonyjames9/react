3b502394f0d88b34a066c0cf09c06353c49fe72c
hipstar-08 <sonyjames9@gmail.com>
2023-05-21 18:14:45 +0530

    Like button

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,9 @@
-import ListGroup from "./components/ListGroup/ListGroup";
-import { BsFillCalendarFill } from "react-icons/bs";
-import Button from "./components/Button/Button";
+import Like from "./components/Like";
 
 function App() {
   return (
     <div>
-      <Button onClick={() => {}}>My Button</Button>
+      <Like onClick={() => console.log("clicked")} />
     </div>
   );
 }
