7af72ae908f1311be8f8046c2dba2c8055317c65
sj9_y_mac <sonyjames9@yahoo.com>
2023-05-31 10:42:36 +0530

    StateObjects and SpreadOperator -	modified:   notes.txt -	modified:   react-app/src/App.tsx

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import Message from "./Message";
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
+
+  const handleClick = () => {
+    setDrink({ ...drink, price: 6 });
+  };
+
   return (
     <div>
-      <Message />
-      <Message />
-      <Message />
+      {drink.price}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
