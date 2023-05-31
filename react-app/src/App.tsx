c4bc74df5d3826a7210e05b21ca0bc27e00563e6
sj9_y_mac <sonyjames9@yahoo.com>
2023-05-31 10:11:33 +0530

    random notes

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,17 @@
-import Like from "./components/Like";
+import { useState } from "react";
 
 function App() {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [person,setPerson] = useState({
+    firstName: '',
+    lastName: ''
+  });
+  const [isLoading, setLoading] = useState(false);
+  
   return (
     <div>
-      <Like onClick={() => console.log("clicked")} />
+      {firstName} {lastName}
     </div>
   );
 }
