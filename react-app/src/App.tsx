a072cacea00634527e6181d259710c2fb2b62a96
sj9_y_mac <sonyjames9@yahoo.com>
2023-06-02 10:20:26 +0530

    - Immer

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,24 +1,23 @@
 import { useState } from "react";
+import produce from "immer";
 
 function App() {
-  const [customer, setCustomer] = useState({
-    name: "John",
-    address: {
-      city: "San Francisco",
-      zipCode: 411018,
-    },
-  });
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug1", fixed: false },
+    { id: 2, title: "Bug2", fixed: false },
+  ]);
 
   const handleClick = () => {
-    setCustomer({
-      ...customer,
-      address: { ...customer.address, zipCode: 560097 },
-    });
+    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    setBugs(produce(draft => {
+      const bug = draft.find(bug => bug.id === 1);
+      if (bug) bug.fixed = true
+    }))
   };
 
   return (
     <div>
-      {customer.address.zipCode}
+      {bugs.map(bug => <p key={bug.id}>{bug.title} {bug.fixed ? 'Fixed': 'New' } </p>)}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
