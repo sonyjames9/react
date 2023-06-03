696696849ea28fc5c9f1dff3abcd2ffca0add237
sj9_y_mac <sonyjames9@yahoo.com>
2023-06-03 17:53:46 +0530

    - spread operator exercise

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "John",
+    },
+  });
+
+  const handleClick = () => {
+    // setGame({...game, player : {name: 'Bob'}})
+    setGame({
+      ...game,
+      player: {
+        ...game.player,
+        name: "Bob",
+      },
+    });
+  };
 
   return (
     <div>
-      <NavBar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
     </div>
   );
 }
