a2818dce94f7575020a8e96915d8e32f5928feef
sj9_y_mac <sonyjames9@yahoo.com>
2023-06-03 19:23:36 +0530

    updating state values for different object

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,28 +1,54 @@
 import { useState } from "react";
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "John",
-    },
+  //     const [game, setGame] = useState({
+  //       id: 1,
+  //       player: {
+  //         name: "John",
+  //       },
+  //     });
+  //
+  //     const handleClick = () => {
+  //       // setGame({...game, player : {name: 'Bob'}})
+  //       setGame({
+  //         ...game,
+  //         player: {
+  //           ...game.player,
+  //          Change player name
+  //           name: "Bob",
+  //         },
+  //       });
+  //     };
+
+  //   const [pizza, setPizza] = useState({
+  //     name: 'Spicy Pepperoni',
+  //     toppings: ['Mushroom']
+  //   });
+  //
+  //   const handleClick = () => {
+  //
+  //     // Add toppings to pizza
+  //     setPizza({ ...pizza, toppings: [...pizza.toppings, 'Cheese']})
+  //   };
+
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product1", quantity: 1 },
+      { id: 2, title: "Product2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    // setGame({...game, player : {name: 'Bob'}})
-    setGame({
-      ...game,
-      player: {
-        ...game.player,
-        name: "Bob",
-      },
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
     });
   };
 
-  return (
-    <div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
