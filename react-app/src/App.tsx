d521a044aa23b4fdf5a9fb65c988e51559d64091
sj9_y_mac <sonyjames9@yahoo.com>
2023-06-04 19:49:13 +0530

    Form Submissions

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,21 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText maxChars={10}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora minus
-        quibusdam quod molestias. Et exercitationem dolore ipsam voluptate neque
-        asperiores, placeat ducimus consequuntur itaque id nulla eligendi omnis,
-        autem eveniet veritatis? Sapiente earum officia voluptas qui debitis
-        aspernatur hic, natus saepe ut similique quam. Facilis sapiente, in sed
-        rem quod, assumenda minus exercitationem nostrum cum quidem impedit non,
-        explicabo adipisci illum molestiae dolor error? Veritatis aliquid totam
-        nulla accusamus ipsam cumque, eius reiciendis in soluta! Necessitatibus
-        rem praesentium voluptate quisquam in fugit dolorem beatae dignissimos
-        aut velit! Quae quos distinctio cumque. Deserunt, earum. Totam a
-        repudiandae alias iste saepe dolores.
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
