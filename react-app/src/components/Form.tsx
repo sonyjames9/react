e6763223a3510e2f915c86a22f675bd47981cd39
sj9_y_mac <sonyjames9@yahoo.com>
2023-06-04 20:17:55 +0530

    useRef usage - return useRef to server

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,9 +1,19 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useRef } from "react";
 
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: "", age: 0 };
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("Submitted");
+    if (nameRef.current !== null)
+      // console.log(nameRef.current.value);
+      person.name = nameRef.current.value;
+    if (ageRef.current !== null)
+      // console.log(ageRef.current.value);
+      person.age = parseInt(ageRef.current.value);
+    console.log(person);
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -11,13 +21,13 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={nameRef} id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={ageRef} id="age" type="number" className="form-control" />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
