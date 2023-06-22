27887a1fffd111c35a1c9eb06d40fe76b85dcd98
sj9_y_mac <sonyjames9@yahoo.com>
2023-06-22 11:48:59 +0530

    loader object

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -9,17 +9,24 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
   useEffect(() => {
     const controller = new AbortController();
+
+    setLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
-      });
+        setLoading(false);
+      })
 
     return () => controller.abort();
   }, []);
@@ -27,6 +34,7 @@ function App() {
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>
