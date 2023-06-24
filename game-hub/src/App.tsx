cbb93375c48936fdc9ba7215100ebddc2e70a137
sj9_y_mac <sonyjames9@yahoo.com>
2023-06-25 00:22:36 +0530

    fetch genres

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,6 +1,7 @@
 import { Button, Grid, GridItem, Show, useColorMode } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   const { colorMode, toggleColorMode } = useColorMode()
@@ -8,26 +9,26 @@ function App() {
   return (
     <header>
       <Button onClick={toggleColorMode}>
-        Toggle {colorMode === 'dark' ? 'Light': 'Dark'}
+        Toggle {colorMode === "dark" ? "Light" : "Dark"}
       </Button>
-    <Grid
-      templateAreas={{
-        base: `"nav"  "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
+      <Grid
+        templateAreas={{
+          base: `"nav"  "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
       >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
-      <Show above="lg">
-        <GridItem area="aside">
-          Aside
+        <GridItem area="nav">
+          <NavBar />
         </GridItem>
-      </Show>
-      <GridItem area="main">
-        <GameGrid />
-      </GridItem>
-    </Grid>
+        <Show above="lg">
+          <GridItem area="aside">
+            <GenreList />
+          </GridItem>
+        </Show>
+        <GridItem area="main">
+          <GameGrid />
+        </GridItem>
+      </Grid>
     </header>
   );
 }
